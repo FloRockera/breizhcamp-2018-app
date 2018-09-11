76832e466508fdde73e8170051e6e074d5a8365a
Flora Calbris <flora.calbris@gmail.com>
2018-09-11 09:47:17 +0200

    Initialisation projet

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('** Application BreizhCamp 2018 **');
+
+//Grâce aux outils de développement du navigateur,
+//déterminer les URLs permettant d'obtenir la liste des sessions au format JSON.
